498e1d376378552d791e589dc56fe4d0f55c4835
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-09-10 17:57:46 +0200

    testing

--- Random/WIP/testTPL.cpp
+++ Random/WIP/testTPL.cpp
@@ -43,8 +43,63 @@ template<typename T> EnableIfT<std::is_floating_point<T>::value, int> fnEifTest(
 	return 2;
 }
 
+template<typename T> class IsCopyAssignable
+{
+	private:
+		template<typename TT, 
+			typename = decltype(std::declval<TT&>() = std::declval<const TT&>())>
+		static std::true_type tryAssignment(TT&&); // Possible SFINAE
+		
+		static std::false_type tryAssignment(...); // Catch-all overload
+
+	public:
+		using Type = decltype(tryAssignment(std::declval<T>()));
+};
+
+
+
+template<typename...> using void_t = void;
+//                 ^ these types have to be well-formed for void_t to instantiate(?)
+//				   (SFINAE-friendly: it either gives `void` or NO ANSWER)
+
+template<typename, typename = void> struct HasHelloTypeMember : std::false_type { };
+template<typename T> struct HasHelloTypeMember<T, void_t<typename T::hello>> : std::true_type { };
+//																  ^
+//		If T::hello does not exist, this is ill-formed and void_t SFINAE kicks in 
+
+template<typename T> using VTCopyAssignmentHelper 
+	= decltype(std::declval<T&>() = std::declval<const T&>);
+template<typename T, typename = void> struct IsVTCopyAssignable : std::false_type { };
+template<typename T> struct IsVTCopyAssignable<T, void_t<VTCopyAssignmentHelper<T>>>
+	: std::is_same<VTCopyAssignmentHelper<T>, T&> { };
+
+
+/*
+// impossible?
+template<typename T, typename = void> struct IsWellFormed : std::false_type { };
+template<typename T> struct IsWellFormed<T, void_t<T>> : std::true_type { };
+
+#define DEFINE_WELLFORMED_CHECKER(mName, ...) \
+	template<typename, typename = void> struct mName : std::false_type { }; \
+	template<typename T> struct mName<T, void_t<T>> : std::true_type { }; \	
+// __________
+*/
+
+
+
+struct TestHello1 { int hello; };
+struct TestHello2 { void hello() { } };
+struct TestHello3 { using hello = int; };
+
 int main() 
 {
+	ssvu::lo() << bool(HasHelloTypeMember<TestHello1>::value) << std::endl;
+	ssvu::lo() << bool(HasHelloTypeMember<TestHello2>::value) << std::endl;
+	ssvu::lo() << bool(HasHelloTypeMember<TestHello3>::value) << std::endl;
+
+	// ssvu::lo() << bool(IsWellFormed<TestHello1::hi>::value) << std::endl;
+	// possible?
+
 	if(false)
 	{
 		ssvu::lo() << int(Abs<-15>::value) << std::endl;
