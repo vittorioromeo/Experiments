f16d217667289982899b6efd2da39fa35c27af6f
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-09-22 21:12:37 +0200

    testing

--- Random/WIP/testManip.cpp
+++ Random/WIP/testManip.cpp
@@ -1,16 +1,110 @@
 #include <SSVUtils/Core/Core.hpp>
 #include <SSVUtils/Benchmark/Benchmark.hpp>
 
+namespace tmi
+{
+	using CoutType = decltype(std::cout);
+	using StdEndLine = CoutType&(CoutType&);
+
+	enum class LCacheType : int {Fmt, Str, FOS, Commit, Manip};
+	
+	class LCache;
+
+	class LCacheItem
+	{
+		friend class LCache;
+
+		private:
+			LCacheType type;
+			union
+			{
+				std::string impl;
+				StdEndLine manip;
+			} u;
+	};
+
+	class LCache
+	{	
+		private:
+			std::vector<LCacheItem> items;
+
+			template<LCacheType TType> inline void append(const std::string& mImpl)
+			{
+				LCacheItem item;
+				item.type = TType;
+				item.impl = mImpl;
+				items.emplace_back(item);
+			}
+
+		public:
+			inline void fmt(const std::string& mImpl)	{ append<LCacheType::Fmt>(mImpl); }
+			inline void str(const std::string& mImpl)	{ append<LCacheType::Str>(mImpl); }
+			inline void fos(const std::string& mImpl)	{ append<LCacheType::FOS>(mImpl); }
+			inline void commit()						{ append<LCacheType::Commit>(""); }
+			inline void manip(StdEndLine mManip)		{ append<LCacheType::Commit>(""); }
+
+			inline void exec(std::ostream& mStream)
+			{
+				auto iC(0u);
+				while(iC < items.size() && items[iC].type != LCacheType::Commit) ++iC;
+				
+
+			}
+
+			/*cache:
+			M   left		|1
+			M   setw		|2
+			F   blue		|0
+			S   number		|3
+			FOS postfix		|4
+			C 				|5*/
+	};
+
+	template<typename T> struct Stringifier;
+	template<> struct Stringifier<unsigned int>
+	{
+		inline void impl(LCache& mCache, const unsigned int& mValue)
+		{
+			mCache.fmt(ssvu::Console::setColorFG(ssvu::Console::Color::Blue));
+			mCache.str(std::to_string(mValue));
+			mCache.fos("u");
+			mCache.commit();
+		}
+	};
+	template<> struct Stringifier<float>
+	{
+		inline void impl(LCache& mCache, const unsigned int& mValue)
+		{
+			mCache.fmt(ssvu::Console::setColorFG(ssvu::Console::Color::Red));
+			mCache.str(std::to_string(mValue));
+			mCache.fos(".f");
+			mCache.commit();
+		}
+	};
+
+
+
+	struct LOut
+	{
+
+	};
+
+	inline auto& lo() noexcept { static LOut result; return result; }
+
+	template<typename T> inline auto& operator<<(LOut& mLOut, const T& mValue)
+	{
+		return mLOut;
+	}
+
+	inline auto& operator<<(LOut& mLOut, StdEndLine mManip)
+	{
+		return mLOut;
+	}
+}
+
 int main()
 {
-	ssvu::lo() << std::left << std::setw(25) << "something"  << "too" << std::endl;
-    ssvu::lo() << std::left << std::setw(25) << "something" << "too" << std::endl; 
-
-    ssvu::lo() << std::left << std::setw(25) << std::string("something")  << "too" << std::endl; 
-    ssvu::lo() << std::left << std::setw(20) << std::string("something")  << std::string("too") << std::endl; 
-    ssvu::lo() << std::left << std::setw(15) << 10ul  << std::setw(15)<< std::string("too") << std::setw(15)<< 5u<< std::setw(15) << 15.f<< std::endl; 
-    //std::cout << std::left << std::setw(25) << ssvu::Console::setColorFG(ssvu::Console::Color::Red) << "something" << "too" << std::endl;
-
+	
 	return 0;
 }
 
@@ -20,7 +114,7 @@ stringifier<long>(number, postfix, cache)
 {
 	cache.append<Fmt>(blue);
 	cache.append<Str>(toStr(number));
-	cache.append<FmtOnlyStr>(postfix);
+	cache.append<FOS>(postfix);
 	cache.append<Commit>();
 }
 
