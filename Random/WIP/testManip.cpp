e56182cc2999c0a2025717606ecd4c9ae415077e
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-09-22 16:19:49 +0200

    tests

--- Random/WIP/testManip.cpp
+++ Random/WIP/testManip.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
-	std::cout << std::left << std::setw(25) << "something"  << "too" << std::endl; 
-    ssvu::lo() << std::left << std::setw(25) << "something" << "too" << std::endl;   
+	ssvu::lo() << std::left << std::setw(25) << "something"  << "too" << std::endl;
+    ssvu::lo() << std::left << std::setw(25) << "something" << "too" << std::endl; 
 
-    std::cout << std::left << std::setw(25) << "something"  << "too" << std::endl; 
-    std::cout << std::left << std::setw(25) << ssvu::Console::setColorFG(ssvu::Console::Color::Red) << "something" << "too" << std::endl;
+    ssvu::lo() << std::left << std::setw(25) << std::string("something")  << "too" << std::endl; 
+    ssvu::lo() << std::left << std::setw(20) << std::string("something")  << std::string("too") << std::endl; 
+    ssvu::lo() << std::left << std::setw(15) << 10ul  << std::setw(15)<< std::string("too") << std::setw(15)<< 5u<< std::setw(15) << 15.f<< std::endl; 
+    //std::cout << std::left << std::setw(25) << ssvu::Console::setColorFG(ssvu::Console::Color::Red) << "something" << "too" << std::endl;
 
 	return 0;
 }
