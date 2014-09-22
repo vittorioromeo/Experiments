1fe3956eb5ed10187ad12c16bc474b847dd72c67
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-09-22 09:01:44 +0200

    testing

--- Random/WIP/testFP.cpp
+++ Random/WIP/testFP.cpp
@@ -5,16 +5,23 @@ int main()
 {
 	auto t1(std::async(std::launch::async, []
 	{
-		ssvu::lo() << "starting t1" << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(2));
-		ssvu::lo() << "finished t1" << std::endl;
+		ssvu::lo("t1") << "starting t1" << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		ssvu::lo("t1") << "finished t1" << std::endl;
 	}));
 
 	auto t2(std::async(std::launch::async, []
 	{
-		ssvu::lo() << "starting t2" << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(2));
-		ssvu::lo() << "finished t2" << std::endl;
+		ssvu::lo("t2") << "starting t2" << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		ssvu::lo("t2") << "finished t2" << std::endl;
+	}));
+
+	auto t3(std::async(std::launch::async, []
+	{
+		ssvu::lo("t3") << "starting t3" << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		ssvu::lo("t3") << "finished t3" << std::endl;
 	}));
 
 	//t1.get();
