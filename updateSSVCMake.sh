87bce8f735f9e9acda7abdc326d26bed3be9f96e
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-14 18:05:48 +0200

    reset

--- updateSSVCMake.sh
+++ updateSSVCMake.sh
@@ -5,6 +5,6 @@ git commit -a -m "progress"
 git push
 
 cd ..
-git ws do "(cd extlibs && cd SSVCMake && git pull origin master)"
+git ws do "(cd extlibs && cd SSVCMake && git reset --hard HEAD && git pull origin master)"
 sudo git ws do "rm ./build/CMakeCache.txt"
 sudo git ws do "rm ./lib/CMakeCache.txt"
