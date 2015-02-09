bee9cc588ea5997fb4121c2743f27b31900d8bf1
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-02-09 22:34:46 +0100

    tests

--- Random/cts.cpp
+++ Random/cts.cpp
@@ -36,16 +36,89 @@ namespace ssvu
 		};
 		template<char T, char... T1s, typename TResult> struct CLTrimExtraZeros<CharList<T, T1s...>, TResult>
 		{
-			using CurrentList = typename CLTrimExtraZeros<CharList<T1s...>, TResult>::Type;
 			using AddedList = typename CLTrimExtraZeros<CharList<T1s...>, typename TResult::template PushBack<T>>::Type;
-
 			using Type = Conditional<T == '\0', typename TResult::template PushBack<'\0'>, AddedList>;
 		};
 
-		template<SizeT TI1, typename, typename> struct CLContainsLHlpr;
-		template<SizeT TI1, char... TCs1, char... TCs2> struct CLContainsLHlpr<TI1, CharList<TCs1...>, CharList<TCs2...>>
-		{
+		// h e l l o w o r l d 0 
+		// ^
+		// l l o 0
+		// ^
 
+		// 0 1 2 3 
+		// h e y
+		// y 
+
+		template<int, int, typename, typename> struct CLContainsLHlpr;
+		template<bool, int, int, typename, typename> struct Test2;
+		template<bool, int, int, typename, typename> struct Test1;
+
+		template<int TI1, int TI2, char... TCs1, char... TCs2> struct Test2<false, TI1, TI2, CharList<TCs1...>, CharList<TCs2...>>
+		{
+			using L1 = CharList<TCs1...>;
+			using L2 = CharList<TCs2...>;
+			static constexpr int sz1{L1::size};
+			static constexpr int sz2{L2::size};
+
+			using Type = typename CLContainsLHlpr<TI1 + 1, TI2 + 1, L1, L2>::Type;
+		};
+		template<int TI1, int TI2, char... TCs1, char... TCs2> struct Test2<true, TI1, TI2, CharList<TCs1...>, CharList<TCs2...>>
+		{
+			using Type = TrueT;
+		};
+
+
+		template<int TI1, int TI2, char... TCs1, char... TCs2> struct Test1<false, TI1, TI2, CharList<TCs1...>, CharList<TCs2...>>
+		{
+			using L1 = CharList<TCs1...>;
+			using L2 = CharList<TCs2...>;
+			static constexpr int sz1{L1::size};
+			static constexpr int sz2{L2::size};
+
+			using Type = Conditional
+			<
+				(L1::template at<TI1>() != L2::template at<TI2>()),
+				typename CLContainsLHlpr<TI1 + 1, 0, L1, L2>::Type,
+				typename Test2<(TI2 == sz2 - 1), TI1, TI2, L1, L2>::Type
+			>;
+		};
+		template<int TI1, int TI2, char... TCs1, char... TCs2> struct Test1<true, TI1, TI2, CharList<TCs1...>, CharList<TCs2...>>
+		{
+			using Type = FalseT;
+		};
+
+		// 0 1 2 3 
+		// h e y
+		// h e y
+
+		
+		template<int TI1, int TI2, char... TCs1, char... TCs2> struct CLContainsLHlpr<TI1, TI2, CharList<TCs1...>, CharList<TCs2...>>
+		{
+			using L1 = CharList<TCs1...>;
+			using L2 = CharList<TCs2...>;
+			static constexpr int sz1{L1::size};
+			static constexpr int sz2{L2::size};
+	
+			using Type = typename Test1<(TI1 >= sz1), TI1, TI2, L1, L2>::Type;
+
+			/*
+			using Type2 = Conditional
+			<
+				(TI1 >= (sz1 - sz2)),
+				FalseT,
+				Conditional
+				<
+					(L1::template at<TI1>() != L2::template at<TI2>()),
+					typename CLContainsLHlpr<TI1 + 1, 0, L1, L2>::Type,
+					Conditional
+					<
+						(TI2 == sz2 - 1),
+						TrueT,
+						typename CLContainsLHlpr<TI1 + 1, TI2 + 1, L1, L2>::Type
+					>
+				>
+			>;			
+			*/
 		};
 
 		template<SizeT, SizeT, char...> struct CLNth;
@@ -57,13 +130,13 @@ namespace ssvu
 	{ 
 		using Type = CharList<TChars...>;
 
-		// TODO: variable templates not supported in gcc 4.9.2, only in 5
+
 
 		static constexpr SizeT size{sizeof...(TChars)};
-		template<SizeT TI> static constexpr char at{Impl::CLNth<0, TI, TChars...>::value};
-		static constexpr char head{size > 0 ? at<0> : '\0'};
-		static constexpr char tail{size > 0 ? at<size - 1> : '\0'};
-		template<char TC> static constexpr bool has{Impl::CLContainsHlpr<TC, TChars...>()};
+		template<SizeT TI> inline static constexpr char at() { return Impl::CLNth<0, TI, TChars...>::value; }
+		static constexpr char head{size > 0 ? at<0>() : '\0'};
+		static constexpr char tail{size > 0 ? at<size - 1>() : '\0'};
+		template<char TC> inline static constexpr bool has() { return Impl::CLContainsHlpr<TC, TChars...>(); }
 		// TODO: template<char TC> static constexpr bool countOf{Impl::CLContainsHlpr<TC, TChars...>()};
 
 		template<char TC> using PushFront = CharList<TC, TChars...>;
@@ -72,7 +145,10 @@ namespace ssvu
 		using PopBack = typename Impl::CLPopBackHlpr<Type>::Type;
 		using TrimExtraZeros = typename Impl::CLTrimExtraZeros<Type, CharList<>>::Type;
 
-		// TODO: template<typename TL> inline static constexpr bool has() noexcept { return Impl::CLContainsLHlpr<Type, TL>(); }
+		template<typename TL> inline static constexpr bool has() noexcept 
+		{ 
+			return typename Impl::CLContainsLHlpr<0, 0, Type::PopBack, typename TL::PopBack>::Type{}(); 
+		}
 
 
 		static constexpr const char cstr[]{TChars...};
@@ -167,31 +243,41 @@ int main()
 	SSVU_ASSERT_STATIC_NM(ssvu::isSame<ssvu::CharList<'h', 'e', 'y'>::PushBack<'u'>, ssvu::CharList<'h', 'e', 'y', 'u'>>());
 	SSVU_ASSERT_STATIC_NM(ssvu::isSame<ssvu::CharList<'h', 'e', 'e', 'y', '\0', '\0', '\0'>::TrimExtraZeros, ssvu::CharList<'h', 'e', 'e', 'y', '\0'>>());
 
-	SSVU_ASSERT_STATIC_NM(ssvu::CharList<'h', 'e', 'y'>::at<0> == 'h');
-	SSVU_ASSERT_STATIC_NM(ssvu::CharList<'h', 'e', 'y'>::at<1> == 'e');
-	SSVU_ASSERT_STATIC_NM(ssvu::CharList<'h', 'e', 'y'>::at<2> == 'y');
+	SSVU_ASSERT_STATIC_NM(ssvu::CharList<'h', 'e', 'y'>::at<0>() == 'h');
+	SSVU_ASSERT_STATIC_NM(ssvu::CharList<'h', 'e', 'y'>::at<1>() == 'e');
+	SSVU_ASSERT_STATIC_NM(ssvu::CharList<'h', 'e', 'y'>::at<2>() == 'y');
 
-	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::at<0> == 'h');
-	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::at<1> == 'e');
-	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::at<2> == 'y');
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::at<0>() == 'h');
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::at<1>() == 'e');
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::at<2>() == 'y');
 
-	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<'h'>);
-	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<'e'>);
-	SSVU_ASSERT_STATIC_NM(!SSVU_CTSTR("hey")::has<'z'>);
-	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<'\0'>);
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<'h'>());
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<'e'>());
+	SSVU_ASSERT_STATIC_NM(!SSVU_CTSTR("hey")::has<'z'>());
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<'\0'>());
 
 	using namespace ssvu::CTLiteral;
-	SSVU_ASSERT_STATIC_NM(decltype("hey"_cts)::at<0> == 'h');
-	SSVU_ASSERT_STATIC_NM(decltype("hey"_cts)::at<1> == 'e');
-	SSVU_ASSERT_STATIC_NM(decltype("hey"_cts)::at<2> == 'y');
+	SSVU_ASSERT_STATIC_NM(decltype("hey"_cts)::at<0>() == 'h');
+	SSVU_ASSERT_STATIC_NM(decltype("hey"_cts)::at<1>() == 'e');
+	SSVU_ASSERT_STATIC_NM(decltype("hey"_cts)::at<2>() == 'y');
 
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<SSVU_CTSTR("h")>());
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<SSVU_CTSTR("e")>());
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<SSVU_CTSTR("y")>());
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<SSVU_CTSTR("he")>());
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<SSVU_CTSTR("ey")>());
+	SSVU_ASSERT_STATIC_NM(SSVU_CTSTR("hey")::has<SSVU_CTSTR("hey")>());
+	SSVU_ASSERT_STATIC_NM(!SSVU_CTSTR("hey")::has<SSVU_CTSTR("z")>());
+	SSVU_ASSERT_STATIC_NM(!SSVU_CTSTR("hey")::has<SSVU_CTSTR("\0")>());
+	SSVU_ASSERT_STATIC_NM(!SSVU_CTSTR("hey")::has<SSVU_CTSTR("hhey")>());
+	SSVU_ASSERT_STATIC_NM(!SSVU_CTSTR("hey")::has<SSVU_CTSTR("")>());
 
 	//using TestT = SSVU_CTSTR_WLEN(5, "hello");
 	using TestT = SSVU_CTSTR("hello");
 
 	ssvu::lo() << std::string{TestT::toStr()} << std::endl;
 
-	using KK = ssvu::CharList<'a', 'b', 'c'>;
+//	using KK = ssvu::CharList<'a', 'b', 'c'>;
 
 	//using hello_world = decltype("Hello world!"_cts);
 	//using hello_world = ssvu::CTString
