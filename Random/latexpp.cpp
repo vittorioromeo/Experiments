b3aac3d8d59b65fd77990b9a2ccfbd914f8c6ce6
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-03-25 12:41:08 +0100

    tests

---
+++ Random/latexpp.cpp
@@ -0,0 +1,270 @@
+#include <SSVUtils/Core/Core.hpp>
+
+std::string directivePrefix{"%!"};
+std::string directiveMacro{"macro"};
+std::string directiveGroup{"group"};
+std::string pOpen{"("};
+std::string pClose{")"};
+
+std::string execPrefix{"!!"};
+std::string groupOpen{"{"};
+std::string groupClose{"}"};
+
+using ssvu::SizeT;
+
+struct Parser
+{
+	SizeT idx{0};
+	const std::string& source;
+
+	Parser(const std::string& mSource) noexcept : source{mSource} 
+	{
+
+	}
+
+	bool isDone() const noexcept { return idx >= source.size(); }
+	char get(SizeT mOffset = 0) const noexcept { return source[idx + mOffset]; }
+
+	void step(SizeT mX = 1) { idx += mX; }
+	void step(const std::string& mX) { idx += mX.size(); }
+	
+	void expect(const std::string& mX)
+	{
+		for(auto i(0u); i < mX.size(); ++i)
+			if(get(i) != mX[i])			
+				throw std::runtime_error("Expected: " + mX);		
+	}
+
+	void skipWhitespace()
+	{
+		while(get() == ' ' || get() == '\t')
+			step();
+	}
+
+	void expectStep(const std::string& mX)
+	{
+		expect(mX);
+		step(mX);
+	}
+
+	bool findNext(const std::string& mX)
+	{		
+		while(!isDone())
+		{
+			while(!isDone() && get() != mX[0]) step();
+			if(!matchAhead(mX)) 
+			{
+				step(mX);
+				continue;
+			}
+					
+			return true;			
+		}
+
+		return false;
+	}
+
+	bool matchAhead(const std::string& mX)
+	{
+		if(idx + mX.size() >= source.size()) return false;
+
+		for(auto i(0u); i < mX.size(); ++i)
+			if(get(i) != mX[i]) return false;
+
+		return true;
+	}
+
+	void stepUntil(const std::string& mX)
+	{	
+		while(!matchAhead(mX))				
+			step();		
+	}
+
+	auto readUntil(const std::string& mX)
+	{
+		std::string result;
+
+		while(!isDone() && !matchAhead(mX))		
+		{			
+			result += get();		
+			step();
+		}
+
+		return result;
+	}
+
+	auto rest() { return source.substr(idx); }
+};
+
+struct LatexPP
+{
+	struct DirectiveMacro
+	{
+		std::string replacement;
+	};
+
+	struct DirectiveGroup
+	{
+		std::string pre, post;
+	};
+
+	std::string source, result;
+	std::map<std::string, DirectiveMacro> macros;
+	std::map<std::string, DirectiveGroup> groups;
+
+	void loadFromPath(const ssvufs::Path& mX)
+	{
+		if(!mX.exists<ssvufs::Type::File>())
+			throw std::runtime_error("Source file path is unexistant");
+
+		source = mX.getContentsAsString();
+	}
+
+	void step0_findDirectives()
+	{		
+		Parser p{source};
+		while(p.findNext(directivePrefix))
+		{			
+			// Skip prefix
+			p.step(directivePrefix);
+
+			if(p.matchAhead(directiveMacro))
+			{
+				// Found macro
+				p.step(directiveMacro);
+
+				// Get its name				
+				p.expectStep(pOpen);				
+				auto name(p.readUntil(pClose));
+				p.expectStep(pClose);
+
+				// Get data
+				DirectiveMacro m;
+				p.expectStep(pOpen);
+				m.replacement = p.readUntil(pClose);
+				p.expectStep(pClose);
+
+				macros[name] = m;
+			}
+			else if(p.matchAhead(directiveGroup))
+			{
+				// Found group
+				p.step(directiveGroup);
+
+				// Get its name				
+				p.expectStep(pOpen);				
+				auto name(p.readUntil(pClose));
+				p.expectStep(pClose);
+
+				// Get data
+				DirectiveGroup g;
+				p.expectStep(pOpen);
+				g.pre = p.readUntil(pClose);
+				p.expectStep(pClose);
+				p.expectStep(pOpen);
+				g.post = p.readUntil(pClose);
+				p.expectStep(pClose);
+
+				groups[name] = g;
+			}
+			else
+			{
+				throw std::runtime_error("Invalid directive");
+			}
+		}
+	}
+
+	bool step1_preprocess()
+	{
+		bool foundSomething{false};
+
+		Parser p{source};
+		while(!p.isDone())
+		{					
+			// Skip DDL
+			if(p.matchAhead(directivePrefix))
+			{
+				p.stepUntil("\n");
+				p.expectStep("\n");
+			}
+			else if(p.matchAhead(execPrefix + pOpen))
+			{
+				foundSomething = true;
+
+				p.step(execPrefix + pOpen);
+				auto name(p.readUntil(pClose));
+				p.expectStep(pClose);
+
+				if(macros.count(name) > 0)
+				{
+					auto& m(macros[name]);
+					result += m.replacement;
+				}
+				else if(groups.count(name) > 0)
+				{
+					auto& g(groups[name]);
+					p.skipWhitespace();
+					p.stepUntil(execPrefix);					
+
+					p.skipWhitespace();
+					p.expectStep(execPrefix + groupOpen);
+					
+					auto body(p.readUntil(execPrefix + groupClose));					
+
+					p.skipWhitespace();
+					p.expectStep(execPrefix + groupClose);
+					
+					result += g.pre;
+					result += body;
+					result += g.post;
+				}
+				else
+				{
+					throw std::runtime_error("Invalid macro/group usage: " + name);
+				}
+			}
+			else
+			{
+				result += p.get();
+				p.step();
+			}
+		}
+
+		return foundSomething;
+	}	
+
+	void execute()
+	{
+		step0_findDirectives();
+
+		// for(const auto& m : macros) ssvu::lo() << m.first << " -> " << m.second.replacement << "\n";
+		// for(const auto& g : groups) ssvu::lo() << g.first << " -> " << g.second.pre << " { body } " << g.second.post << "\n";
+
+		bool foundSomething{true};
+		while(foundSomething) 
+		{
+			result = "";
+			foundSomething = step1_preprocess();
+			source = result;
+		}
+
+		std::cout << result << std::endl;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{
+		ssvu::lo("Error") << "LatexPP requires the source file path as an argument" << std::endl;
+		return 1;
+	}
+
+	std::string sourcePath{argv[1]};
+
+	LatexPP lpp;
+	lpp.loadFromPath(sourcePath);
+	lpp.execute();
+
+	return 0;
+}
---
+++ Random/stoi.cpp
@@ -0,0 +1,223 @@
+#include <SSVUtils/Core/Core.hpp>
+#include <SSVUtils/Benchmark/Benchmark.hpp>
+
+constexpr int times{40000};
+
+inline auto& getSS() noexcept { thread_local std::ostringstream oss; return oss; }
+template<typename T> inline auto ss(T x) { getSS().str(""); getSS() << x; return getSS().str(); }
+
+template<typename T> inline auto getri() noexcept { return ssvu::getRndI<T, T>(ssvu::NumLimits<T>::min(), ssvu::NumLimits<T>::max()); }
+
+template<typename T> inline void check(T v, const std::string& mX)
+{
+	//if(mX != std::to_string(v) || mX != ss(v))
+	{
+		//ssvu::lo() << mX << ", " << std::to_string(v) << ", " << ss(v) << "\n";
+	}
+}
+
+namespace ssvu
+{
+	namespace Impl
+	{
+		namespace NumToStr
+		{
+			template<SizeT TS> inline auto& getConvBuffer() noexcept
+			{
+				thread_local std::array<char, TS> result;
+				return result;
+			}
+
+			template<typename T, class TEnable = void> struct Conv;
+
+			constexpr char digits[201]
+			{
+				"00010203040506070809"
+				"10111213141516171819"
+				"20212223242526272829"
+				"30313233343536373839"
+				"40414243444546474849"
+				"50515253545556575859"
+				"60616263646566676869"
+				"70717273747576777879"
+				"80818283848586878889"
+				"90919293949596979899"
+			};
+
+			template<typename T> struct Conv<T, EnableIf<!isUnsigned<T>()>>
+			{
+				static constexpr SizeT bufferSize{NumLimits<T>::digits10};
+
+				inline static auto toStr(T val) noexcept
+				{					
+					auto& buf(getConvBuffer<bufferSize>());
+					char* it{&buf[bufferSize - 2]};
+
+					if(val >= 0) 
+					{
+						auto div(val / 100);
+
+						while(div) 
+						{
+							std::memcpy(it,&digits[2 * (val - div * 100)], 2);
+							val = div;
+							it -= 2;
+							div = val / 100;
+						}
+						
+						std::memcpy(it, &digits[2 * val], 2);					
+						if(val < 10) it++;
+					} 
+					else 
+					{
+						auto div(val / 100);
+
+						while(div) 
+						{
+							std::memcpy(it, &digits[-2 * (val - div * 100)], 2);
+							val = div;
+							it -= 2;
+							div = val / 100;
+						}
+						
+						std::memcpy(it, &digits[-2 * val], 2);						
+						if(val <= -10) it--;
+						*it = '-';
+					}
+
+					return std::string(it, &buf[bufferSize] - it);
+				}
+			};
+
+			template<typename T> struct Conv<T, EnableIf<isUnsigned<T>()>>
+			{
+				static constexpr SizeT bufferSize{NumLimits<T>::digits10};
+
+				inline static auto toStr(T val) noexcept
+				{
+					auto& buf(getConvBuffer<bufferSize>());
+					char* it{&buf[bufferSize - 2]};
+
+					auto div(val / 100);
+
+					while(div) 
+					{
+						std::memcpy(it, &digits[2 * (val - div * 100)], 2);
+						val = div;
+						it -= 2;
+						div = val / 100;
+					}
+
+					std::memcpy(it, &digits[2 * val], 2);
+					if(val < 10) it++;
+
+					return std::string(it, &buf[bufferSize] - it);
+				}
+			};
+
+
+		
+			template<typename T> inline auto toStr(const T& mX) noexcept
+			{
+				return Conv<T>::toStr(mX);
+			}	
+		}
+	}	
+}
+
+
+
+
+
+
+
+
+
+
+template<typename T> inline std::string veenaivei(T x) noexcept
+{
+	return ssvu::Impl::NumToStr::toStr(x);
+}
+
+
+template<typename T> inline void runTest() noexcept
+{	
+	{
+		SSVU_BENCHMARK_RUN_GROUP_SCOPE_EXIT("stringstream");
+
+auto min(ssvu::NumLimits<T>::min());
+			auto max(ssvu::NumLimits<T>::max());
+			check(min, ss(min));
+			check(max, ss(max));
+
+		for(int i = 0; i < times; ++i)
+		{
+			auto t(getri<T>());
+			
+
+			check(t, ss(t));
+		}
+	}
+
+	{
+		SSVU_BENCHMARK_RUN_GROUP_SCOPE_EXIT("to_string");
+
+auto min(ssvu::NumLimits<T>::min());
+			auto max(ssvu::NumLimits<T>::max());
+			check(min, std::to_string(min));
+			check(max, std::to_string(max));
+
+		for(int i = 0; i < times; ++i)
+		{
+			auto t(getri<T>());
+			
+
+			check(t, std::to_string(t));
+		}
+	}
+
+	{
+		SSVU_BENCHMARK_RUN_GROUP_SCOPE_EXIT("vee");
+
+auto min(ssvu::NumLimits<T>::min());
+			auto max(ssvu::NumLimits<T>::max());
+			check(min, veenaivei(min));
+			check(max, veenaivei(max));
+
+		for(int i = 0; i < times; ++i)
+		{
+			auto t(getri<T>());
+			
+
+			check(t, veenaivei(t));
+		}
+	}
+}
+
+
+
+int main() 
+{
+
+
+	SSVU_BENCHMARK_INIT_GROUP_SCOPE_EXIT("stringstream");
+	SSVU_BENCHMARK_INIT_GROUP_SCOPE_EXIT("to_string");
+	SSVU_BENCHMARK_INIT_GROUP_SCOPE_EXIT("vee");
+
+	runTest<int>();
+	runTest<unsigned int>();
+
+
+	runTest<short>();
+	runTest<long long>();
+	runTest<unsigned short>();
+	
+	runTest<long>();
+	runTest<unsigned long>();
+	runTest<unsigned long long>();
+
+
+
+
+	return 0;
+}
