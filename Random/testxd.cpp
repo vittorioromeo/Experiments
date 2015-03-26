ed6f1ad40060f522b83850b655307c6963cd6454
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-03-26 11:43:32 +0100

    damn

--- Random/testxd.cpp
+++ Random/testxd.cpp
@@ -1,8 +1,24 @@
+#include <vector>
+#include <memory>
+#include <iostream>
 #include <SSVUtils/Core/Core.hpp>
 
+struct XTBase
+{
+	inline virtual ~XTBase() { }
+};
+struct XTDer : public XTBase
+{
+	int n{1};
+	inline int f(int mK) const { return mK + n; }
+};
 
 int main()
-{
-	ssvu::lo() << "";
+{	
+	using namespace ssvu;
+
+	std::vector<UPtr<XTBase>> v;
+	v.emplace_back(mkUPtr<XTDer>());
+
 	return 0;
 }
