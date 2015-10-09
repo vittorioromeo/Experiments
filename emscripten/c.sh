142dabf1ecf108aba1931ed173105635d29ab2b1
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-10-09 19:48:25 +0200

    sh

--- emscripten/c.sh
+++ emscripten/c.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-/usr/lib/emscripten/em++ -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_LIBPNG=1 -s DEMANGLE_SUPPORT=1 \
+/usr/lib/emscripten/em++ -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 -s USE_LIBPNG=1 -s DEMANGLE_SUPPORT=1 \
     --shell-file /usr/lib/emscripten/src/shell_minimal.html \
-    --preload-file ./files/** \
+    --preload-file ./files/ \
     -std=c++14 \
     -o main.html ./main.cpp \
 && (chromium http://localhost:8080/main.html)
