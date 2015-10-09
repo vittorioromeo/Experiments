3bd2fbe452c40060fe95edafb7aa86bcaa19cd82
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-10-09 18:09:13 +0200

    i love c++14

--- emscripten/c.sh
+++ emscripten/c.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-/usr/lib/emscripten/em++ \
+/usr/lib/emscripten/em++ -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_LIBPNG=1 -s DEMANGLE_SUPPORT=1 \
     --shell-file /usr/lib/emscripten/src/shell_minimal.html \
     --preload-file ./files/** \
     -std=c++14 \
--- emscripten/main.cpp
+++ emscripten/main.cpp
@@ -1,14 +1,80 @@
 #include <stdio.h>
 #include <iostream>
-#include <SDL/SDL.h>
+#include <SDL.h>
 #include <SDL_image.h>
 #include <emscripten.h>
 #include <unordered_map>
 #include <string>
 #include <cassert>
+#include <type_traits>
 
 namespace sdl
 {
+    template <typename T>
+    class vec2
+    {
+    private:
+        T _x, _y;
+
+    public:
+        vec2() noexcept : _x{T(0)}, _y{T(0)} {};
+        vec2(T x, T y) noexcept : _x{x}, _y{y} {}
+
+        vec2(const vec2&) = default;
+        vec2& operator=(const vec2&) = default;
+
+        vec2(vec2&&) = default;
+        vec2& operator=(vec2&&) = default;
+
+        auto& x() noexcept { return _x; }
+        const auto& x() const noexcept { return _x; }
+
+        auto& y() noexcept { return _y; }
+        const auto& y() const noexcept { return _y; }
+
+        template <typename TV>
+        vec2& operator+=(const TV& v) noexcept
+        {
+            _x += v._x;
+            _y += v._y;
+            return *this;
+        }
+
+        template <typename TV>
+        vec2& operator-=(const TV& v) noexcept
+        {
+            _x -= v._x;
+            _y -= v._y;
+            return *this;
+        }
+
+        template <typename TS>
+        vec2& operator*(TS s) noexcept
+        {
+            _x *= s;
+            _y *= s;
+            return *this;
+        }
+
+        template <typename TS>
+        vec2& operator/(TS s) noexcept
+        {
+            _x /= s;
+            _y /= s;
+            return *this;
+        }
+    };
+
+    using vec2i = vec2<int>;
+    using vec2u = vec2<unsigned int>;
+    using vec2f = vec2<float>;
+
+    template <typename T0, typename T1>
+    auto make_vec2(T0 x, T1 y)
+    {
+        return vec2<std::common_type_t<T0, T1>>{x, y};
+    }
+
     using key_code = SDL_Keycode;
     using mouse_coord = Sint32;
     using mouse_btn = Uint8;
@@ -64,6 +130,90 @@ namespace sdl
                 });
             return result;
         }
+
+        template <typename T, typename TDeleter>
+        class unique_resource
+        {
+        private:
+            T* _ptr{nullptr};
+            TDeleter deleter;
+
+        public:
+            unique_resource() = default;
+            unique_resource(T* p) : _ptr{p}
+            {
+                if(_ptr == nullptr)
+                {
+                    impl::log_sdl_error();
+                    std::terminate();
+                }
+            }
+
+            unique_resource(const unique_resource&) = delete;
+            unique_resource& operator=(const unique_resource&) = delete;
+
+            unique_resource(unique_resource&& s) : _ptr{s._ptr}
+            {
+                s._ptr = nullptr;
+            }
+            unique_resource& operator=(unique_resource&& s)
+            {
+                _ptr = s._ptr;
+                s._ptr = nullptr;
+                return *this;
+            }
+
+            ~unique_resource()
+            {
+                if(_ptr != nullptr) deleter(_ptr);
+            }
+
+
+            auto ptr() noexcept
+            {
+                assert(_ptr != nullptr);
+                return _ptr;
+            }
+            const auto ptr() const noexcept
+            {
+                assert(_ptr != nullptr);
+                return _ptr;
+            }
+
+            auto& get() noexcept { return *ptr(); }
+            const auto& get() const noexcept { return *ptr(); }
+
+            operator T*() noexcept { return ptr(); }
+        };
+
+        struct unique_window_deleter
+        {
+            void operator()(SDL_Window* p) noexcept { SDL_DestroyWindow(p); }
+        };
+
+        struct unique_texture_deleter
+        {
+            void operator()(SDL_Texture* p) noexcept { SDL_DestroyTexture(p); }
+        };
+
+        struct unique_renderer_deleter
+        {
+            void operator()(SDL_Renderer* p) { SDL_DestroyRenderer(p); }
+        };
+
+        struct unique_surface_deleter
+        {
+            void operator()(SDL_Surface* p) { SDL_FreeSurface(p); }
+        };
+
+        using unique_window =
+            unique_resource<SDL_Window, unique_window_deleter>;
+        using unique_texture =
+            unique_resource<SDL_Texture, unique_texture_deleter>;
+        using unique_renderer =
+            unique_resource<SDL_Renderer, unique_renderer_deleter>;
+        using unique_surface =
+            unique_resource<SDL_Surface, unique_surface_deleter>;
     }
 
     class context;
@@ -90,58 +240,26 @@ namespace sdl
 
         auto mouse_x() const noexcept { return _mouse_x; }
         auto mouse_y() const noexcept { return _mouse_y; }
+        auto mouse_pos() const noexcept
+        {
+            return make_vec2(mouse_x(), mouse_y());
+        }
     };
 
     class image;
+    class sprite;
 
-    class surface
+    class surface : public impl::unique_surface
     {
     private:
-        SDL_Surface* _surface{nullptr};
+        using base_type = impl::unique_surface;
 
     public:
-        surface() = default;
-        surface(SDL_Surface* p) : _surface{p}
-        {
-            if(_surface == nullptr)
-            {
-                impl::log_sdl_error();
-                std::terminate();
-            }
-        }
-
-        surface(const surface&) = delete;
-        surface& operator=(const surface&) = delete;
-
-        surface(surface&& s) : _surface{s._surface} { s._surface = nullptr; }
-        surface& operator=(surface&& s)
-        {
-            _surface = s._surface;
-            s._surface = nullptr;
-            return *this;
-        }
-
-        ~surface()
-        {
-            if(_surface != nullptr) SDL_FreeSurface(_surface);
-        }
-
-        auto ptr() noexcept
-        {
-            assert(_surface != nullptr);
-            return _surface;
-        }
-        const auto ptr() const noexcept
-        {
-            assert(_surface != nullptr);
-            return _surface;
-        }
-
-        auto& get() noexcept { return *ptr(); }
-        const auto& get() const noexcept { return *ptr(); }
+        using base_type::base_type;
 
         auto width() const noexcept { return get().w; }
         auto height() const noexcept { return get().h; }
+        auto size() const noexcept { return make_vec2(width(), height()); }
 
         auto format() const noexcept { return get().format; }
 
@@ -156,7 +274,10 @@ namespace sdl
             SDL_FillRect(ptr(), &clear_rect, SDL_MapRGB(format(), r, g, b));
         }
 
-        void display() { SDL_Flip(ptr()); }
+        void display()
+        {
+            // SDL_Flip(ptr());
+        }
 
         void blit(const surface& s, const SDL_Rect& src_rect,
             const SDL_Rect& dest_rect)
@@ -165,7 +286,7 @@ namespace sdl
                 s.ptr(), &src_rect, ptr(), &const_cast<SDL_Rect&>(dest_rect));
         }
 
-        void blit(const image& i, const SDL_Rect& dest_rect);
+        void blit(const image& i, int x, int y);
     };
 
     class image
@@ -177,6 +298,7 @@ namespace sdl
     public:
         auto width() const noexcept { return _surface.width(); }
         auto height() const noexcept { return _surface.height(); }
+        auto size() const noexcept { return _surface.size(); }
 
         image(const std::string& path) : _surface{IMG_Load(path.c_str())}
         {
@@ -192,9 +314,79 @@ namespace sdl
         const auto& rect() const noexcept { return _rect; }
     };
 
-    void surface::blit(const image& i, const SDL_Rect& dest_rect)
+    class texture : public impl::unique_texture
     {
-        blit(i.surface(), i.rect(), dest_rect);
+    private:
+        using base_type = impl::unique_texture;
+
+    public:
+        using base_type::base_type;
+    };
+
+    class renderer : public impl::unique_renderer
+    {
+    private:
+        using base_type = impl::unique_renderer;
+
+    public:
+        using base_type::base_type;
+    };
+
+
+    class window : public impl::unique_window
+    {
+    private:
+        using base_type = impl::unique_window;
+
+    public:
+        using base_type::base_type;
+    };
+
+
+
+    struct space_data
+    {
+        vec2f _pos, _size, _origin;
+        float _radians{0.f};
+
+        space_data() = default;
+
+        space_data(const space_data&) = default;
+        space_data& operator=(const space_data&) = default;
+
+        space_data(space_data&&) = default;
+        space_data& operator=(space_data&&) = default;
+    };
+
+    void surface::blit(const image& i, int x, int y)
+    {
+        blit(i.surface(), i.rect(), SDL_Rect{x, y, i.width(), i.height()});
+    }
+
+    class sprite
+    {
+    private:
+        image* _image{nullptr};
+        space_data _sd;
+
+    public:
+        sprite() = default;
+        sprite(image& i) noexcept : _image{&i} {}
+
+        sprite(const sprite& s) = default;
+        sprite& operator=(const sprite& s) = default;
+
+        sprite(sprite&& s) = default;
+        sprite& operator=(sprite&& s) = default;
+    };
+
+    void fillTexture(SDL_Renderer* renderer, SDL_Texture* texture, int r, int g,
+        int b, int a)
+    {
+        SDL_SetRenderTarget(renderer, texture);
+        SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+        SDL_SetRenderDrawColor(renderer, r, g, b, a);
+        SDL_RenderFillRect(renderer, nullptr);
     }
 
     class context
@@ -203,7 +395,11 @@ namespace sdl
         const std::size_t _width;
         const std::size_t _height;
 
-        surface _screen;
+        window _window;
+        renderer _renderer;
+        texture _texture;
+
+        // surface _screen;
         SDL_Event _event;
 
         key_event_handler _on_key_down{impl::null_key_event_handler()};
@@ -259,32 +455,75 @@ namespace sdl
         void run_update() { update_fn()(1.f); }
         void run_draw()
         {
-            _screen.clear(0, 0, 0);
-
+            // _screen.clear(0, 0, 0);
             draw_fn()();
 
-            _screen.display();
+
+            SDL_SetRenderTarget(_renderer, nullptr);
+            SDL_RenderClear(_renderer);
+            fillTexture(_renderer, _texture, 255, 0, 0, 255);
+            SDL_RenderCopy(_renderer, _texture, nullptr, nullptr);
+            SDL_RenderPresent(_renderer);
+
+            //_screen.display();
         }
 
     public:
         context(std::size_t width, std::size_t height)
             : _width{width}, _height{height}
         {
+
+            /*
             if(SDL_Init(SDL_INIT_VIDEO) != 0)
             {
                 impl::log_sdl_error();
                 std::terminate();
             }
+            */
 
+            /*
             constexpr int img_flags{IMG_INIT_PNG};
             if(!(IMG_Init(img_flags) & img_flags))
             {
                 impl::log_img_error();
                 std::terminate();
             }
+            */
 
-            _screen =
-                surface{SDL_SetVideoMode(_width, _height, 32, SDL_SWSURFACE)};
+
+            _window = SDL_CreateWindow(
+                "emscripten window", 0, 0, _width, _height, SDL_WINDOW_OPENGL);
+
+            if(_window == nullptr)
+            {
+                impl::log_img_error();
+                std::terminate();
+            }
+
+            _renderer = SDL_CreateRenderer(_window, -1,
+                SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+
+            if(_renderer == nullptr)
+            {
+                impl::log_img_error();
+                std::terminate();
+            }
+
+            _texture = SDL_CreateTexture(_renderer, SDL_PIXELFORMAT_ARGB8888,
+                SDL_TEXTUREACCESS_TARGET, _width, _height);
+
+            if(_texture == nullptr)
+            {
+                impl::log_img_error();
+                std::terminate();
+            }
+
+            SDL_SetRenderTarget(_renderer, nullptr);
+            SDL_SetRenderDrawBlendMode(_renderer, SDL_BLENDMODE_BLEND);
+            SDL_SetRenderDrawColor(_renderer, 128, 128, 128, 255);
+
+            //            _screen = surface{SDL_SetVideoMode(_width, _height,
+            //            32, SDL_SWSURFACE)};
 
             SDL_GL_SetSwapInterval(0);
 
@@ -309,7 +548,10 @@ namespace sdl
             };
         }
 
-        ~context() { SDL_Quit(); }
+        ~context()
+        {
+            SDL_Quit();
+        }
 
         void run()
         {
@@ -318,11 +560,12 @@ namespace sdl
             run_draw();
         }
 
-        auto& screen() noexcept { return _screen; }
-        const auto& screen() const noexcept { return _screen; }
+        // auto& screen() noexcept { return _screen; }
+        // const auto& screen() const noexcept { return _screen; }
 
         auto mouse_x() const noexcept { return _input_state.mouse_x(); }
         auto mouse_y() const noexcept { return _input_state.mouse_y(); }
+        auto mouse_pos() const noexcept { return _input_state.mouse_pos(); }
 
         auto key(key_code c) const { return _input_state.key(c); }
         auto btn(mouse_btn b) const { return _input_state.btn(b); }
@@ -338,23 +581,16 @@ namespace sdl
 int main(int argc, char** argv)
 {
     std::cout << "Hello world!\n";
-    sdl::context c{640, 480};
+    sdl::context c{1000, 480};
     sdl::init_global_context(c);
 
-    sdl::image toriel{"files/toriel.png"};
+    /*sdl::image toriel{"files/toriel.png"};
 
     c.draw_fn() = [&c, &toriel]
     {
-        SDL_Rect dest;
 
-        dest.x = c.mouse_x();
-        dest.y = c.mouse_y();
-        dest.w = toriel.width();
-        dest.h = toriel.height();
-
-        // c.screen().blit(toriel.surface(), src, dest);
-        c.screen().blit(toriel, dest);
-    };
+        c.screen().blit(toriel, c.mouse_x(), c.mouse_y());
+    };*/
 
 
 
