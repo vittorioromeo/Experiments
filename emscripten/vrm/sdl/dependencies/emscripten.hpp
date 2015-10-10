5a3ba973850145270a7c760192a3e71fc27637a3
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-10-10 17:47:49 +0200

    deps

---
+++ emscripten/vrm/sdl/dependencies/emscripten.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-2016 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+// http://vittorioromeo.info | vittorio.romeo@outlook.com
+
+#pragma once
+
+#include <emscripten.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL_opengles2.h>
---
+++ emscripten/vrm/sdl/dependencies/regular.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2015-2016 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+// http://vittorioromeo.info | vittorio.romeo@outlook.com
+
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL2/SDL_opengles2.h>
