4332776ad9849aa1d43e1f247449816afc9096b2
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-12-12 20:45:06 +0100

    remove SSVUT_RUN

--- SSVVM/src/SSVVM/main.cpp
+++ SSVVM/src/SSVVM/main.cpp
@@ -4,152 +4,150 @@
 
 std::string source{
     R"(
-	//!ssvasm
+    //!ssvasm
 
-	$require_registers(4);
+    $require_registers(4);
 
-	$define(R0,			0);
-	$define(R1,			1);
-	$define(R2,			2);
-	$define(ROutput,	3);
-
-
-
-
-	// _______________________________
-	//	FN_MAIN function
-	//		* entrypoint
-	//		* returns in ROutput
-	// _______________________________
-
-	$label(FN_MAIN);
-
-		// Compute the 6th fibonacci number
-
-		// Load constants
-		loadIntCVToR(R0, 6);
-
-		// Save registers
-		pushRVToS(R0);
-		pushRVToS(R1);
-
-		// Push args
-		pushRVToS(R0);
-
-		// Call func
-		callPI(FN_FIB);
-
-		// Get return value
-		moveRVToR(ROutput, R0);
-
-		// Pop args
-		popSV();
-
-		// Restore registers
-		popSVToR(R1);
-		popSVToR(R0);
-
-
-
-		// Push output to stack
-		pushRVToS(ROutput);
-
-		halt();
+    $define(R0,			0);
+    $define(R1,			1);
+    $define(R2,			2);
+    $define(ROutput,	3);
 
 
 
 
-	// _______________________________
-	//	FN_FIB function
-	//		* needs 1 int argument
-	//		* uses R0, R1
-	//		* returns in R0
-	// _______________________________
+    // _______________________________
+    //	FN_MAIN function
+    //		* entrypoint
+    //		* returns in ROutput
+    // _______________________________
 
-	$label(FN_FIB);
+    $label(FN_MAIN);
 
-		// Get arg from stack
-		moveSBOVToR(R0, 2);
+        // Compute the 6th fibonacci number
 
-		// Check if arg is < 2 (put compare result in R1)
-		compareIntRVIntCVToR(R1, R0, 2);
+        // Load constants
+        loadIntCVToR(R0, 6);
 
-		// Return arg if arg < 2
-		goToPIIfCompareRVSmaller(FN_FIB_RET_ARG, R1);
+        // Save registers
+        pushRVToS(R0);
+        pushRVToS(R1);
 
+        // Push args
+        pushRVToS(R0);
 
+        // Call func
+        callPI(FN_FIB);
 
+        // Get return value
+        moveRVToR(ROutput, R0);
 
-		// Else return fib(arg - 1) + fib(arg - 2)
+        // Pop args
+        popSV();
 
-		// Calculate fib(arg - 1)
-
-			// Save registers
-			pushRVToS(R0);
-
-			// Push args
-			pushIntCVToS(1);
-			pushRVToS(R0);
-			subtractInt2SVs();
-
-			// Call func
-			callPI(FN_FIB);
-
-			// Get return value
-			// Return value is in R0, move it to R2
-			moveRVToR(R2, R0);
-
-			// Pop args
-			popSV();
-
-			// Restore registers
-			popSVToR(R0);
-
-			// Push fib(arg - 1) on stack
-			pushRVToS(R2);
-
-		// Calculate fib(arg - 2)
-			// Save registers
-			pushRVToS(R0);
-
-			// Push args
-			pushIntCVToS(2);
-			pushRVToS(R0);
-			subtractInt2SVs();
-
-			// Call func
-			callPI(FN_FIB);
-
-			// Get return value
-			// Return value is in R0, move it to R2
-			moveRVToR(R2, R0);
-
-			// Pop args
-			popSV();
-
-			// Restore registers
-			popSVToR(R0);
-
-			// Push fib(arg - 2) on stack
-			pushRVToS(R2);
-
-		// Return fib(arg - 1) + fib(arg + 1)
-			addInt2SVs();
-			popSVToR(R0);
-			returnPI();
+        // Restore registers
+        popSVToR(R1);
+        popSVToR(R0);
 
 
 
-		$label(FN_FIB_RET_ARG);
-			returnPI();
-	)"};
+        // Push output to stack
+        pushRVToS(ROutput);
+
+        halt();
+
+
+
+
+    // _______________________________
+    //	FN_FIB function
+    //		* needs 1 int argument
+    //		* uses R0, R1
+    //		* returns in R0
+    // _______________________________
+
+    $label(FN_FIB);
+
+        // Get arg from stack
+        moveSBOVToR(R0, 2);
+
+        // Check if arg is < 2 (put compare result in R1)
+        compareIntRVIntCVToR(R1, R0, 2);
+
+        // Return arg if arg < 2
+        goToPIIfCompareRVSmaller(FN_FIB_RET_ARG, R1);
+
+
+
+
+        // Else return fib(arg - 1) + fib(arg - 2)
+
+        // Calculate fib(arg - 1)
+
+            // Save registers
+            pushRVToS(R0);
+
+            // Push args
+            pushIntCVToS(1);
+            pushRVToS(R0);
+            subtractInt2SVs();
+
+            // Call func
+            callPI(FN_FIB);
+
+            // Get return value
+            // Return value is in R0, move it to R2
+            moveRVToR(R2, R0);
+
+            // Pop args
+            popSV();
+
+            // Restore registers
+            popSVToR(R0);
+
+            // Push fib(arg - 1) on stack
+            pushRVToS(R2);
+
+        // Calculate fib(arg - 2)
+            // Save registers
+            pushRVToS(R0);
+
+            // Push args
+            pushIntCVToS(2);
+            pushRVToS(R0);
+            subtractInt2SVs();
+
+            // Call func
+            callPI(FN_FIB);
+
+            // Get return value
+            // Return value is in R0, move it to R2
+            moveRVToR(R2, R0);
+
+            // Pop args
+            popSV();
+
+            // Restore registers
+            popSVToR(R0);
+
+            // Push fib(arg - 2) on stack
+            pushRVToS(R2);
+
+        // Return fib(arg - 1) + fib(arg + 1)
+            addInt2SVs();
+            popSVToR(R0);
+            returnPI();
+
+
+
+        $label(FN_FIB_RET_ARG);
+            returnPI();
+    )"};
 
 
 
 int main()
 {
-    SSVUT_RUN();
-
     auto src(ssvvm::SourceVeeAsm::fromStrRaw(source));
     ssvvm::preprocessSourceRaw<true>(src);
     auto program(ssvvm::getAssembledProgram<true>(src));
